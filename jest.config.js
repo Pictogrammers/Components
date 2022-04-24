3b796832390079934c3e27a411dfbf9d363f415a
Austin Andrews <admin@templarian.com>
2022-04-24 13:27:07 -0500

    Revemo duplicates.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.html$": "./jest/htmlLoader.js",
-    "^.+\\.css$": "./jest/cssLoader.js"
-  },
-  "setupFiles": [
-    "<rootDir>/node_modules/document-register-element/build/document-register-element.js"
-  ],
-  "testEnvironment": "jsdom"
-}
+  preset: '@pictogrammers/element-jest'
+};
