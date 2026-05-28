32a3c873fdd0d7c9b950897881c68ef1d1f3a06b
Austin Andrews <admin@templarian.com>
2026-05-27 22:13:54 -0500

    progress

--- src/components/pg/nodes/nodes.ts
+++ src/components/pg/nodes/nodes.ts
@@ -1,4 +1,5 @@
 import { Component, Prop, Part, forEach } from '@pictogrammers/element';
+import { NodeConnector } from '@pictogrammers/node-connector';
 
 import PgNode from '../node/node';
 import PgNodeEntry from '../nodeEntry/nodeEntry';
@@ -14,15 +15,22 @@ import style from './nodes.css';
 export default class PgNodes extends HTMLElement {
   @Prop() items: any = [];
 
+  @Part() $svg: SVGSVGElement;
   @Part() $items: HTMLDivElement;
   @Part() $forceScroll: HTMLDivElement;
 
   connectedCallback() {
+    const connector = new NodeConnector(this.$svg);
     forEach({
       container: this.$items,
       items: this.items,
       type: (item) => item.node === 0 ? PgNodeEntry : PgNode,
       create: ($item, item) => {
+        connector.setNode(`${item.node}`, item.x * 16, item.y * 16, 192, 128);
+        if (item.node !== 0) {
+          connector.setInputPin(`${item.node}`, 'in', 16);
+        }
+        connector.setOutputPin(`${item.node}`, 'out', 16);
         $item.addEventListener('registernode', this.#registerNode.bind(this));
       },
     });
