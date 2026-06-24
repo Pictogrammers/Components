c983726ef825c27d94ed60afba9f781b45288c35
BittuBarnwal7479 <bk8379903@gmail.com>
2026-06-25 05:25:28 +0530

    feat(color): add copy button to basic example

--- src/components/pg/color/__examples__/basic/basic.ts
+++ src/components/pg/color/__examples__/basic/basic.ts
@@ -1,4 +1,4 @@
-import { Component, Part, Prop } from '@pictogrammers/element';
+import { Component, Part } from '@pictogrammers/element';
 import PgColor from '../../color';
 
 import template from './basic.html';
@@ -10,11 +10,38 @@ import template from './basic.html';
 export default class XPgColorBasic extends HTMLElement {
   @Part() $color1: PgColor;
   @Part() $colorSelected: HTMLSpanElement;
+  @Part() $copyButton: HTMLButtonElement;
+
+  private selectedHex = '';
 
   connectedCallback() {
     this.$color1.addEventListener('select', (e: CustomEvent) => {
       const { rgb, hex } = e.detail;
+
+      this.selectedHex = hex;
       this.$colorSelected.innerText = `${hex} or rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+      this.$copyButton.disabled = false;
+    });
+
+    this.$copyButton.addEventListener('click', async () => {
+      try {
+        await navigator.clipboard.writeText(this.selectedHex);
+
+        const original = this.$copyButton.innerText;
+        this.$copyButton.innerText = 'Copied!';
+
+        setTimeout(() => {
+          this.$copyButton.innerText = original;
+        }, 1000);
+      } catch (error) {
+        console.error('Failed to copy color:', error);
+
+        this.$copyButton.innerText = 'Failed';
+
+        setTimeout(() => {
+          this.$copyButton.innerText = 'Copy';
+        }, 1000);
+      }
     });
   }
 }
